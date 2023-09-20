ae8667bab65f5ea6d0eee76e2bd064f187ea62df
li-kuan-gi <ie31415926@gmail.com>
2023-09-20 17:51:52 +0800

    define args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
+use args::{Args, Commands};
+use clap::Parser;
 use pngsecret::result::Result;
 
+mod args;
+
 fn main() -> Result<()> {
-    todo!()
+    let args = Args::parse();
+
+    match &args.command {
+        Commands::Encode {
+            file_path,
+            chunk_type,
+            message,
+            output_file,
+        } => {
+            let default = "default".to_string();
+            let output_file = output_file.as_ref().unwrap_or(&default);
+            let s = format!(
+                "cmd: {}, path: {}, type: {}, msg: {}, output: {}",
+                "encode", file_path, chunk_type, message, output_file
+            );
+            println!("{}", s);
+        }
+        Commands::Decode {
+            file_path,
+            chunk_type,
+        } => println!(
+            "cmd: {}, path: {}, type: {}",
+            "decode", file_path, chunk_type
+        ),
+        Commands::Remove {
+            file_path,
+            chunk_type,
+        } => println!(
+            "cmd: {}, path: {}, type: {}",
+            "remove", file_path, chunk_type
+        ),
+        Commands::Print { file_path } => println!("cmd: {}, path: {}", "print", file_path),
+    }
+    Ok(())
 }
